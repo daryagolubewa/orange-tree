ffe5cd2aedd05b6d6c68bdcab1d27665fe1e7ecb
daryagolubeva <darya.golubewa@gmail.com>
2018-10-16 15:09:04 +0300

    orangesTree() is made + tests

--- orange_tree.js
+++ orange_tree.js
@@ -1,31 +1,71 @@
 class OrangeTree {
-
-
-  passGrowingSeason(){
-
+  constructor(age, height, oranges = []) {
+    this.age = age; 
+    this.height = height;
+    this.oranges = oranges;
   }
 
+  passGrowingSeason() {
+    
+      if(this.isMature()) {
+        if(this.hasOranges()){
+          this.oranges = [];
+        }
+        let random = Math.floor(Math.random() * (300 - 100) + 100); 
+        for(let i = 0; i <= random; i++) {
+            this.oranges.push(new Orange());
+        }
+      }
+      this.age++;
+      if(this.height <= 22.5){
+        this.height += 2.5 
+      }
+    console.log(this.age)
+    console.log(this.height)
+    console.log(this.oranges)
+
+  }
 
   //  Returns true if the tree is old enough to bear fruit (6), false otherwise
-  mature(){
+  isMature(){
+    if (this.age >= 6) {
+      return true
+    } else {
+      return false
+    }
 
   }
 
+  isDead() {
+    if(this.age >= 100) {
+      return true;
+    } 
+      return false
+  }
 
   //  Returns +true+ if there are any oranges on the tree, +false+ otherwise
   hasOranges(){
-
+    if (this.isMature()) {
+      if (this.oranges.length > 0) {
+        return true
+      } 
+    } 
+    return false;
   }
 
 
   //  Returns an Orange if there are any
   //  Raises a NoOrangesError otherwise
   pickAnOrange() {
-    if(!this.hasOranges){
-      throw Error("This tree has no oranges");
+    if(!this.hasOranges()){
+      return "This tree has no oranges"
+    } else {
+      return this.oranges.shift();
+      
     }
   }
 
     //  orange-picking logic goes here
 
 }
+
--- spec/orange_tree_spec.js
+++ spec/orange_tree_spec.js
@@ -9,77 +9,102 @@ describe("OrangeTree", () => {
 
   // You'll need to switch `xdescribe( o `describe` when you're ready
   //  to implement each set of tests.
-
   describe("age", () => {
     it("has an age", () => {
-
+      tree1 = new OrangeTree(15, 25)
+      expect(tree1.age).toEqual(15);
     });
   });
+
 
   describe("height", () => {
     it("has a height", () => {
-
+      tree1 = new OrangeTree(15, 25)
+      expect(tree1.height).toEqual(25);
     });
   });
 
 
-  xdescribe("passGrowingSeason", () => {
+  describe("passGrowingSeason", () => {
+    tree1 = new OrangeTree(1, 25)
+    testTreeAge = tree1.age
     it("should change the tree age", () => {
+      tree1.passGrowingSeason();
+      expect(tree1.age).toBeTruthy(tree1.age !== testTreeAge);
 
     });
 
+    tree2 = new OrangeTree(1, 10)
+    testTreeHeight = tree2.height
     it("should make the tree grow", () => {
-      // This should be more explicit. How much should the tree grow?
+      tree2.passGrowingSeason();
+      expect(tree2.height).toBeTruthy(tree2.height !== testTreeHeight);
+
     });
 
-    // If the tree is old enough to bear fruit
-    it("should cause the tree to produce oranges", () => {
 
+    //If the tree is old enough to bear fruit
+    it("should cause the tree to produce oranges", () => {
+      tree2 = new OrangeTree(1, 10, 140)
+      let orangesAmount = tree2.oranges.length;
+        expect(tree2.oranges).toBeTruthy(orangesAmount > 0);
     });
 
   });
 
-  xdescribe("isMature", () => {
+  describe("isMature", () => {
     it("returns true if tree is old enough to bear fruit", () => {
-
+      tree1 = new OrangeTree(15, 25)
+      expect(tree1.isMature()).toEqual(true);
     });
 
     it("returns false if tree is not old enough to bear fruit", () => {
-
+      let tree1 = new OrangeTree(5, 10)
+      expect(tree1.isMature()).toEqual(false);
     });
   });
 
-  xdescribe("isDead", () => {
+  describe("isDead", () => {
     it("should return false for an alive tree", () => {
-
+      tree1 = new OrangeTree(101, 25)
+      expect(tree1.isDead()).toEqual(true);
     });
 
     it("should return true for a dead tree", () => {
+      tree1 = new OrangeTree(15, 25)
+      expect(tree1.isDead()).toEqual(false);
 
     });
   });
 
-  xdescribe("hasOranges", () => {
+  describe("hasOranges", () => {
     it("should return true if oranges are on the tree", () => {
-
+      tree1 = new OrangeTree(15, 25, [1, 1])
+      expect(tree1.hasOranges()).toEqual(true);
     });
 
     it("should return false if the tree has no oranges", () => {
-
+      tree1 = new OrangeTree(15, 25, [])
+      expect(tree1.hasOranges()).toEqual(false);
     });
   });
 
-  xdescribe("pick_an_orange", () => {
+  describe("pickAnOrange", () => {
     it("should return an orange from the tree", () => {
-
+      tree1 = new OrangeTree(15, 25, [1, 1])
+      expect(tree1.pickAnOrange()).toEqual(1);
     });
 
     it("the returned orange should no longer be on the tree", () => {
-
+      tree1 = new OrangeTree(15, 25, [1, 1])
+      let quantOfOranges = tree1.oranges.length
+      expect(tree1.pickAnOrange()).toBeTruthy(quantOfOranges === 1);
     });
 
     it("should raise an error if the tree has no oranges", () => {
-
+      tree1 = new OrangeTree(15, 25)
+      expect(tree1.pickAnOrange()).toEqual("This tree has no oranges");
     });
   });
+
 });
